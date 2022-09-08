3ac2afa2fa44185d1e449b27dbc7e03fde709450
Tuovi <tuovi.niskanen@siili.com>
2022-09-08 14:25:18 +0300

    Added comments

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 type DropDownProps = {
   breeds: string[];
   showDropDown: boolean;
+  // toggelDropDown is not being used in the DropDown component. It is declared here as a prop,
+  // but it's not passed into the actual component or used in the component so it is not combined with any click event => it
+  // is not doing anything.
+  // also this is not the correct way to declare a function type. Use () => void or () => string etc.
   toggleDropDown: Function;
   breedSelection: Function;
 };
@@ -19,13 +23,28 @@ const DropDown: React.FC<DropDownProps> = ({
    *
    * @param breed  The selected breed
    */
+
+  // Are you sure about this breeds param? Should it be breed maybe? I mean I don't know, but be consistent with naming: breeds
+  // is not a string, breeds is an arrays of strings and you probably don't want to set the breed to all breeds.
   const onClickHandler = (breeds: string): void => {
     breedSelection(breeds);
   };
-
+  // What was the idea here? This is listening to the showDropDown boolean and always setting it to true. Also again, you are
+  // setting the showDropDown in too many places
   useEffect(() => {
     setShowDropDown(showDropDown);
   }, [showDropDown]);
+
+  // If you want to use the bootstrap dropdown, you can use it here in the return, remeber to import it in this file.
+  // For example
+  /* return (
+    <>
+      <Dropdown (the bootstrap one)>
+        <{breeds.map} .............
+      </Dropdown>
+    </>
+  )*/
+  // And then your this file and its component could be called CoreysDropdown or something like that
 
   return (
     <>
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,21 +1,49 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
+// You are not using this Dropdown anywhere. And if you're gonna have your own component for the Dropdown,
+// consider giving it its own name instead of the only difference being the camel casing in your own component.
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+//Component diles should begin with a capital letter and be named in the same way as the actual component.
+//So in this case the file name would be DropDown (note the camel case)
 import DropDown from "./dropdown";
 import { JsxElement } from "typescript";
 import { AnyARecord } from "dns";
 
+//All components should be in their own files and exported from there and imported in the file that is using them.
+// So this input.tsx file should only contain the following:
+/* function Input() {
+  return (
+    <div>
+      <Heading title="Breed"></Heading>
+      <SelectBreed />
+    </div>
+  );
+}
+
+export default Input; */
+
+//Move this into its own Heading.tsx file
 function Heading({ title }: { title: string }) {
   return <h2>{title}</h2>;
 }
-
+// Why use the old function declarement? Prefer the arrow function as you have done in dropdown.tsx
+//Move this component into its own SelectBreed.tsx file
 function SelectBreed() {
+  // you have this state with the same name in two files. It's confusing for a human to understand what you wanted and will most
+  // definitely lead to computer having problems too. No one can never know which one you meant in which use case
   const [showDropDown, setShowDropDown] = useState<boolean>(false);
   const [selectBreed, setSelectBreed] = useState<string>("");
+  // breeds is an array, not a function that returns an array. So const breeds = ['breed1', 'breed2']
   const breeds = () => {
     return ["breed_1", "breed_2"];
   };
+
+  // This toggleDropDown and dismissHandler together is confusing. You are doing almost the same thing in two functions.
+  // Think what you want to achieve. If you just want to toggle the state, the toggleDropdown should be enough
+  // When you look into this and maybe redo the whole dropdown system, really try to think what each click event should do
+  // And try not to call and set the same state in many places. It's hard to read and understand and in a working project
+  // would lead to difficulties in maintaining the project. Prefer using one function over and over that sets the state
 
   const toggleDropDown = () => {
     setShowDropDown(!showDropDown);
@@ -63,7 +91,11 @@ function SelectBreed() {
       >
         {showDropDown && (
           <DropDown
+            // Because breeds needs to be an array, this will not be a call
             breeds={breeds()}
+            // Why is this false instead of showDropDown boolean? If it is set to false, the showDropDown boolean you have passed
+            //from dropdown.tsx will always be false. Do you want it to be always false? In that case, there would be no need to pass it on
+            // You could just use it in the component file
             showDropDown={false}
             toggleDropDown={(): void => toggleDropDown()}
             breedSelection={breedSelection}
